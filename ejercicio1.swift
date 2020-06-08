b53b1920afd45b43188b1aeb9688de20ce8cb093
edmayen <ed_mayen@hotmail.com>
2020-06-07 22:55:03 -0500

    First commit

---
+++ ejercicio1.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+var name: String = "Euardo"
+var lastName: String = "Mayen"
+var age: Int = 23
+var height: Float = 1.82
+var occupation: String = "estudiante"
+var zodiacSign: String = "Sagitario"
+var about: String = "me gusta programar"
+
+var phrase: String = "Hola, nombre es \(name) \(lastName), tengo \(age) años y mido \(height) metros. Mi signo zodiacal es \(zodiacSign). Soy \(occupation) y \(about)."
+
+print(phrase)
